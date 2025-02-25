9692258ae3e96d2ca4f5bc3ad4be70b09f625478
D4nte-web <murugvo261998@gmail.com>
2025-02-25 22:07:19 +0300

    HW3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+    byte a = 100;
+    short b = 1000;
+    int c = 200000;
+    long d = 100000000;
+    float e = 3.5f;
+    double f = 35.4;
 
 
 
